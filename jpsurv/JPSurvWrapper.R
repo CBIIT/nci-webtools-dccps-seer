0b89d6484df31d2dc20d460137f426cc6e2e9ee1
Phyllip Cho <phyllip3@users.noreply.github.com>
2019-12-02 12:24:32 -0500

    minYear condition

--- jpsurv/JPSurvWrapper.R
+++ jpsurv/JPSurvWrapper.R
@@ -613,9 +613,10 @@ getGraphWrapper <- function (filePath, jpsurvDataString, first_calc, com, interv
       return(results)
     }
   } else if (type == 'time') {
-      years = jpsurvData$additional$yearOfDiagnosis
-      params = jpsurvData$calculate$form
-      seerdata = outputData[['seerdata']]
+    years = jpsurvData$additional$yearOfDiagnosis
+    params = jpsurvData$calculate$form
+    seerdata = outputData[['seerdata']]
+    if (length(params$cohortVars) > 0) {
       for (i in 1:length(params$cohortVars)) {
         cohortVar = getCorrectFormat(params$cohortVars[i])
         cohortVal = removeEscape(params$cohortValues[i])
@@ -625,14 +626,19 @@ getGraphWrapper <- function (filePath, jpsurvDataString, first_calc, com, interv
       if (minYear > years) {
         years = minYear
       }
-      graph = plot.surv.int.multiyears(graphData, fit, nJP, yearVar, obscumvar, predcumvar, interval, year.select = years)
-      graphFile = paste(filePath, paste("plot_Int-", jpsurvData$tokenId,"-",com,"-",nJP,"-",iteration,".png", sep=""), sep="/")
-      ggsave(file=paste(filePath, paste("plot_Int-", jpsurvData$tokenId,"-",com,"-",nJP,"-",iteration,".png", sep=""), sep="/"))
-      graphData = scaleTo(graphData)
-      graphData = graphData[graphData[[yearVar]] %in% years,]
+    }
+    graph = plot.surv.int.multiyears(graphData, fit, nJP, yearVar, obscumvar, predcumvar, interval, year.select = years)
+    graphFile = paste(filePath, paste("plot_Int-", jpsurvData$tokenId,"-",com,"-",nJP,"-",iteration,".png", sep=""), sep="/")
+    ggsave(file=paste(filePath, paste("plot_Int-", jpsurvData$tokenId,"-",com,"-",nJP,"-",iteration,".png", sep=""), sep="/"))
+    graphData = scaleTo(graphData)
+    graphData = graphData[graphData[[yearVar]] %in% years,]
+    if (exists("minYear")) {
       results = list("timeGraph" = graphFile, "timeTable" = graphData, "minYear" = minYear)
+    } else {
+      results = list("timeGraph" = graphFile, "timeTable" = graphData)
+    }
+    return(results)
   }
-  return(results)
 }
 
 removeEscape = function(str) {
