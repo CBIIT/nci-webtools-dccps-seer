85e733b1f31207016fccc1bc354fe416f8570438
Phyllip Cho <phyllip3@users.noreply.github.com>
2020-03-24 18:08:25 -0400

    revise plot

--- jpsurv/js/plots.js
+++ jpsurv/js/plots.js
@@ -2,16 +2,21 @@
 function plotLineChart(x, yMark, yLine, dimension, plotTitle, xTitle, yTitle, divID) {
   var mTrace = {};
   var lTrace = {};
+  var legend = {};
   var data = [];
   var uniqueDimensions = Array.from(new Set(dimension));
   var layout = {
     title: plotTitle,
+    hovermode: 'closest',
     xaxis: {
       title: xTitle
     },
     yaxis: {
-      title: yTitle,
-      tickformat: '.2%'
+      title: `${yTitle}\n\t\n\t`,
+      showline: true,
+      tickformat: '%',
+      range: [0, 1],
+      autorange: false
     },
     height: 600,
     width: 900
@@ -33,33 +38,86 @@ function plotLineChart(x, yMark, yLine, dimension, plotTitle, xTitle, yTitle, di
     mTrace[interval] = {
       x: [],
       y: [],
+      showlegend: false,
+      text: [],
+      textposition: 'top center',
+      hoverinfo: 'text',
       mode: 'markers',
       type: 'scatter',
-      marker: { color: colors[i % 10] }
+      marker: { color: colors[i % 10] },
+      legendgroup: interval
     };
+
     lTrace[interval] = {
       x: [],
       y: [],
+      showlegend: false,
+      text: [],
+      hoverinfo: 'text',
       mode: 'lines',
       line: { shape: 'spline' },
       type: 'scatter',
-      line: { color: colors[i % 10] }
+      line: { color: colors[i % 10] },
+      legendgroup: interval
+    };
+
+    legend[interval] = {
+      x: [null],
+      y: [null],
+      showlegend: true,
+      mode: 'lines+markers',
+      type: 'scatter',
+      line: { color: colors[i % 10] },
+      name: divID != 'timePlot' ? interval + '-year ' + jpsurvData.additional.statistic : interval,
+      legendgroup: interval
     };
   });
 
   x.forEach(function(x, i) {
+    var markerText =
+      divID != 'timePlot'
+        ? 'Year at Diagnosis: ' +
+          x +
+          '<br>Recorded Survival: ' +
+          yMark[i].toPrecision(4) +
+          '%' +
+          '<br>Interval: ' +
+          dimension[i]
+        : 'Interval: ' +
+          x +
+          '<br>Recorded Survival: ' +
+          yMark[i].toPrecision(4) +
+          '%' +
+          '<br>Year: ' +
+          dimension[i];
+
+    var lineText =
+      divID != 'timePlot'
+        ? 'Year at Diagnosis: ' +
+          x +
+          '<br>Predicted Survival: ' +
+          yLine[i].toPrecision(4) +
+          '%' +
+          '<br>Interval: ' +
+          dimension[i]
+        : 'Interval: ' +
+          x +
+          '<br>Predicted Survival: ' +
+          yLine[i].toPrecision(4) +
+          '%' +
+          '<br>Year: ' +
+          dimension[i];
+
     mTrace[dimension[i]].x.push(x);
     mTrace[dimension[i]].y.push(yMark[i] / 100);
-    mTrace[dimension[i]].name = 'Recorded - ' + dimension[i];
-    mTrace[dimension[i]].legendgroup = dimension[i];
+    mTrace[dimension[i]].text.push(markerText);
 
     lTrace[dimension[i]].x.push(x);
     lTrace[dimension[i]].y.push(yLine[i] / 100);
-    lTrace[dimension[i]].name = 'Predicted - ' + dimension[i];
-    lTrace[dimension[i]].legendgroup = dimension[i];
+    lTrace[dimension[i]].text.push(lineText);
   });
 
-  [mTrace, lTrace].map(function(traceGroup) {
+  [mTrace, lTrace, legend].map(function(traceGroup) {
     Object.keys(traceGroup).forEach(function(trace) {
       data.push(traceGroup[trace]);
     });
