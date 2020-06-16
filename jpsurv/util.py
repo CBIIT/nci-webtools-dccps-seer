b1eb396e2a977222b180cfceacac0bba055f2c81
Phyllip Cho <phyllip3@users.noreply.github.com>
2020-06-16 16:26:42 -0400

    use configparser

--- jpsurv/util.py
+++ jpsurv/util.py
@@ -1,11 +1,11 @@
 import os
-from configparser import SafeConfigParser
+from configparser import ConfigParser
 from zipfile import ZipFile, ZIP_DEFLATED
 
 
 class Util:
     def __init__(self, filename):
-        config = SafeConfigParser()
+        config = ConfigParser()
         config.read(filename)
 
         # Mail settings
