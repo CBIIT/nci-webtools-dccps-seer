5860f7380449d9cbafbfa572383c771e3e7e29eb
Phyllip Cho <phyllip3@users.noreply.github.com>
2024-05-15 11:13:21 -0400

    fix control visibility

--- server/js/jpsurv.js
+++ server/js/jpsurv.js
@@ -1846,9 +1846,8 @@ export function loadResults(results) {
   setIntervalsDynamic();
 
   const conditional = jpsurvData.calculate.form.conditional;
-  $('#conditionalRecalcVis').toggleClass('d-none', conditional == true);
-
   const relaxProp = jpsurvData.calculate.form.relaxProp;
+  $('#conditionalRecalcVis').toggleClass('d-none', conditional || relaxProp);
   $('#toggleCondControl').toggleClass('d-none', !relaxProp || +jpsurvData.cutPointIndex == 1);
   $('#cutpoint-display-control').toggleClass('d-none', !relaxProp);
   $('#toggleConditionalView').prop('checked', jpsurvData.viewConditional);
