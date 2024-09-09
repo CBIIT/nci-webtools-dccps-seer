891ddd745ce87e8cbad0c3e1600c37078e6e19ea
Phyllip Cho <phyllip3@users.noreply.github.com>
2024-09-09 15:11:58 -0400

    update next config

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: process.env?.APP_PATH || "",
   async rewrites() {
     return [
       {
         source: "/api/:path*",
-        destination: 'http://localhost:9000/api/:path*',
+        destination: `${process.env.API_BASE_URL || "http://localhost:9000"}/api/:path*`,
       },
     ];
   },
