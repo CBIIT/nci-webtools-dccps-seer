89fc01152ba5ec7b5b4d614440c883b04f401c9a
Phyllip Cho <phyllip3@users.noreply.github.com>
2025-10-01 15:23:16 -0400

    add adobe launch

--- client/src/app/layout.jsx
+++ client/src/app/layout.jsx
@@ -30,10 +30,13 @@ export default function RootLayout({ children }) {
         <meta name="keywords" content="jpsurv" />
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <GoogleAnalytics id="G-DGFTR3EY14" />
-        <Script src="https://cbiit.github.io/nci-softwaresolutions-elements/components/include-html.js" />
+        <script
+          src="https://assets.adobedtm.com/6a4249cd0a2c/785de09de161/launch-70d67a6a40a8.min.js"
+          async=""></script>
+        {/* <Script src="https://cbiit.github.io/nci-softwaresolutions-elements/components/include-html.js" /> */}
       </head>
       <body>
-        <include-html src="https://cbiit.github.io/nci-softwaresolutions-elements/banners/government-shutdown.html"></include-html>
+        {/* <include-html src="https://cbiit.github.io/nci-softwaresolutions-elements/banners/government-shutdown.html"></include-html> */}
         <Header routes={routes} />
         <main
           className="position-relative d-flex flex-column flex-grow-1 align-items-stretch bg-light"
