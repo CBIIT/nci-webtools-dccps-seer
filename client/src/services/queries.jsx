ad96b1bd8b7ffd748c72a323f820ac58528318c5
Phyllip Cho <25577714+phyllip3@users.noreply.github.com>
2026-06-08 14:45:25 -0400

    add file size error

--- client/src/services/queries.jsx
+++ client/src/services/queries.jsx
@@ -35,6 +35,9 @@ export async function recalculateConditional(id, params) {
 }
 
 export async function importWorkspace(id, [file]) {
+  if (file.size > 10 * 1024 * 1024) {
+    throw new Error("Workspace file exceeds the 10MB size limit.");
+  }
   const formData = new FormData();
   formData.append("files", file, file.name);
   return await axios.post(`/api/import/${id}`, formData);
